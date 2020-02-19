995cfc707365b7d1296776f95c0bb2cbc6ede009
Maksim Pavlov <pavlovmark100@gmail.com>
2020-02-20 00:50:24 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-
 exports.min = function min (array) {
-  return 0;
-}
+    if (!arguments.length || array.length === 0) {
+        return 0
+    }
+   return Math.min.apply(0, array);
+};
 
 exports.max = function max (array) {
-  return 0;
-}
-
+    if (!arguments.length || array.length === 0) {
+        return 0
+    }
+    return Math.max.apply(0, array);
+};
 exports.avg = function avg (array) {
-  return 0;
-}
+    if (!arguments.length || array.length === 0) {
+        return 0
+    }
+    return (array.reduce((a,b) => a + b)) / array.length;
+};
+
+// console.log(min([9,5,-4,17,-4,-32,4,34,9,38,-32,22,20,33,-17,8,30,12,-17,32]));
